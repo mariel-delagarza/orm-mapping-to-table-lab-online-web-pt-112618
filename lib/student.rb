72d71a35ff4f6ce4701cdf0418261463360e2b4d
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-02-14 01:45:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,14 +1,14 @@
 class Student
-  attr_accessor :name, :grade, id: 
+  attr_accessor :name, :grade, id:
 
   def initialize(id=nil, name, grade)
     @id = id
-    @name = name 
+    @name = name
     @grade = grade
-  end 
+  end
 
   def self.create_table
-    sql = <<-SQL 
+    sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT,
@@ -16,18 +16,18 @@ class Student
       )
       SQL
     DB[:conn].execute(sql)
-  end 
+  end
 
   def self.drop_table
-    sql = <<-SQL 
+    sql = <<-SQL
       DROP TABLE students
       SQL
-    
-    DB[:conn].execute(sql)
-  end 
 
-  def save 
-    sql = <<-SQL 
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES (?,?)
     SQL
@@ -35,15 +35,15 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
 
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-  end 
+  end
 
   def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save
-    student 
-  end 
+    student
+  end
 
 
 
-  
+
 end
